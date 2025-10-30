a6c3931b3a201242cc498fb7b233967df8f0a0e1
Miguel Ángel Delgado <miguelexepg@gmail.com>
2025-10-30 22:37:34 +0100

    Componetizando

---
+++ react/src/components/JobListings.jsx
@@ -0,0 +1,10 @@
+export function JobListings () {
+    return ( 
+        <>
+        <h2>Resultados de búsqueda</h2>
+
+        <div className="jobs-listings">  
+        </div>
+        </>
+    )
+}
